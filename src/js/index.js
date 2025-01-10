2ab12c7db5ea184d465db10d2ea9da74d4b73361
unemakoto <une-m2@asahi.com>
2025-01-10 17:23:38 +0900

    sample15完成

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,9 @@ import "../css/style.css";
 import { WebGLRenderer, Scene, PerspectiveCamera, PlaneGeometry, ShaderMaterial, Mesh, AxesHelper, DoubleSide } from "three";
 import viewport from "./viewport";
 import loader from "./loader";
-import planeSphereVertexGlsl from "./glsl/planeSphere/vertex.glsl";
-import planeSphereFragmentGlsl from "./glsl/planeSphere/fragment.glsl";
+import switchTexVertexGlsl from "./glsl/switchTex/vertex.glsl";
+import switchTexFragmentGlsl from "./glsl/switchTex/fragment.glsl";
 import GUI from "lil-gui";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -39,7 +38,7 @@ async function init() {
   // レンダラーの設定
   world.renderer.setSize(canvasRect.width, canvasRect.height, false);
   world.renderer.setPixelRatio(window.devicePixelRatio);
-  // world.renderer.setPixelRatio(1); // iMacのsafariでスクロールがガタつくため1に落としてみた
+  // world.renderer.setPixelRatio(1); // iMacのsafariでスクロールがガタつく
   world.renderer.setClearColor(0x000000, 0);
 
   // シーン、カメラの作成
@@ -60,64 +59,17 @@ async function init() {
     const texes = await loader.getTexByElement(el); // 先にコール
     const rect = el.getBoundingClientRect(); // awaitの後で実行
 
-    // 平面の分割数をC4Dと合わせておく
-    // const plane_geometry = new PlaneGeometry(rect.width, rect.height, 8, 8);
-    const plane_geometry = new PlaneGeometry(rect.width, rect.height, 20, 10);
-
-    // geometryの時点でx軸周りに90度回転させておく
-    plane_geometry.rotateX(Math.PI / 2);
-
-    // gltfをロード
-    const gltfLoader = new GLTFLoader();
-    let gltf_geometry = null;
-    // gltfはpublic/models/配下に置いた
-    const gltf_file_path = import.meta.env.VITE_HTML_PATH + '/models/twist06.gltf';
-
-    await new Promise((resolve, reject) => {
-      gltfLoader.load(
-        // '/models/twist06.gltf', // glTFファイルのパスを指定
-        gltf_file_path,
-        (gltf) => {
-          const _mesh = gltf.scene.children[0]; // 最初のメッシュを取得
-          if (_mesh && _mesh.geometry) {
-            gltf_geometry = _mesh.geometry; // ジオメトリを取得
-          } else {
-            console.error("GLTFモデルにジオメトリが含まれていません");
-            reject(new Error("No geometry found in GLTF model"));
-          }
-  
-          // もし小さく表示される場合はx,y,z方向に100倍して対応
-          // gltf_geometry.scale(100, 100, 100);
-
-          resolve();
-        },
-        undefined,
-        (error) => {
-          console.error('GLTF読み込みエラー:', error);
-          reject(error);
-        }
-      );
-    });
-
-    // 「position」に初期状態（ここではplane）を設定しておく（three.js仕様？）
-    plane_geometry.setAttribute('position', plane_geometry.getAttribute('position'));
-    // 「uv」に初期状態（ここではplane）を設定しておく
-    // （ここでは頂点の遅延をvertex.glslで設定しているため初期状態のuvも設定しておかないとダメだった）
-    plane_geometry.setAttribute('uv', plane_geometry.getAttribute('uv'));
-
-    // 初期ジオメトリとして平面のジオメトリを設定
-    plane_geometry.setAttribute('initGeometry', plane_geometry.getAttribute('position'));
-    // 最終ジオメトリとして巻物のジオメトリを設定
-    plane_geometry.setAttribute('finalGeometry', gltf_geometry.getAttribute('position'));
+    // メッシュは相棒DOMと同じサイズを指定
+    const geometry = new PlaneGeometry(rect.width, rect.height);
 
     // data-webglの属性値を取得
     const dataWebgl = el.getAttribute('data-webgl');
     console.log(dataWebgl);
 
-    if(dataWebgl == "planeSphere"){
+    if(dataWebgl == "switchTex"){
       material = new ShaderMaterial({
-        vertexShader: planeSphereVertexGlsl,
-        fragmentShader: planeSphereFragmentGlsl,
+        vertexShader: switchTexVertexGlsl,
+        fragmentShader: switchTexFragmentGlsl,
         side: DoubleSide,
         uniforms: {
           uProgress: { value: 0.0 },
@@ -128,7 +80,7 @@ async function init() {
       });
     }
     else {
-      console.error("指定のエフェクト名(data-webgl属性)が見つかりませんでした");
+      console.warn("指定のエフェクト名(data-webgl属性)が見つかりませんでした");
     }
 
     // 複数のテクスチャ画像をuniform変数に設定
@@ -143,23 +95,11 @@ async function init() {
       const gui = new GUI();
       // ON,OFFを切り替えるためのオブジェクト（初期値off）
       const isActive = { value: false };
-      // const folder1 = gui.addFolder("画像切り替え");
-      const folder2 = gui.addFolder("OrbitControls");
-
-      // // [folder1] 画像切り替え。0.0～1.0までの範囲で0.1刻みで動かせるようにする
-      // folder1.add(material.uniforms.uProgress, "value", 0.0, 1.0, 0.1).name('mixの割合').listen();
-      // const mix_check_box_data = {mixCheckBoxVal: Boolean(material.uniforms.uProgress.value)};
-      // folder1.add(mix_check_box_data, "mixCheckBoxVal").name('mixの割合（checkbox）').onChange(() => {
-      //   gsap.to(material.uniforms.uProgress, {
-      //     value: Number(mix_check_box_data.mixCheckBoxVal),
-      //     duration: 1.0,
-      //     ease: "none"
-      //   });
-      // });
+      const folder1 = gui.addFolder("OrbitControls");
     
-      // [folder2] OrbitControlsのチェックボックス
+      // [folder1] OrbitControlsのチェックボックス
       // .onChange()はlil-guiの仕様。isActive.valueに変化があったら引数のコールバックを実行
-      folder2.add(isActive, "value").name('OrbitControlsのON/OFF').onChange(() => {
+      folder1.add(isActive, "value").name('OrbitControlsのON/OFF').onChange(() => {
         if (isActive.value) {
           _attachOrbitControl();
           // Axisの表示もついでにここでする
@@ -180,8 +120,7 @@ async function init() {
     }
     // stats.js（ここまで）-----------------------
 
-    const mesh = new Mesh(plane_geometry, material);
-    mesh.rotation.x = -Math.PI / 2;
+    const mesh = new Mesh(geometry, material);
     world.scene.add(mesh);
     // メッシュ位置を相棒DOMの座標に合わせる
     const { x, y } = getWorldPosition(rect, canvasRect);
@@ -191,7 +130,7 @@ async function init() {
     // 取得したメッシュ情報をオブジェクトにまとめておく
     const obj = {
       mesh,
-      plane_geometry,
+      geometry,
       material,
       rect,
       $: { el },
@@ -208,21 +147,20 @@ async function init() {
   await Promise.all(prms);
 
   // initInview()相当の処理（ここから）---------
-  // 対象となるメッシュは複数個を想定するためループで回す
+  // ScrollTriggerを利用したscrub操作のアニメーション
   for (let i = 0; i < obj_array.length; i++) {
-    gsap.to(obj_array[i].material.uniforms.uProgress, {
-      value: 1.0, // 遷移後の値
-      duration: 2.0,
-      // ease: "none",
-      ease: "power4.out",
-      scrollTrigger: {
-        trigger: obj_array[i].$.el,
-        start: "center 60%",
-        // toggleActions: "play reverse play reverse",
-        toggleActions: "play pause pause reverse",
-        markers: true  // デバッグ用にマーカーを表示
-      }
-    });
+    if (obj_array[i].dataWebgl == "switchTex") {
+      gsap.to(obj_array[i].material.uniforms.uProgress, {
+        value: 1.0, // 遷移後の値
+        scrollTrigger: {
+          trigger: obj_array[i].$.el, // スクロール操作のトリガーとなる要素
+          start: "top 80%",          // アニメーション開始位置
+          end: "bottom 20%",         // アニメーション終了位置
+          scrub: true,               // スクロールに連動させる
+          markers: true              // デバッグ用にマーカーを表示
+        }
+      });
+    }
   }
   // initInview()相当の処理（ここまで）---------
 
@@ -240,7 +178,7 @@ async function init() {
     }
 
     if (window.debug) statsJsControl?.begin(); // fpsの計測（ここから）
-    world.renderer.render(world.scene, world.camera); // 元の記述を消す
+    world.renderer.render(world.scene, world.camera);
     if (window.debug) statsJsControl?.end(); // fpsの計測（ここまで）
   }
 }
@@ -262,7 +200,6 @@ function updateMeshPosition(mesh_obj) {
 function getWorldPosition(dom, canvas) {
   const x = (dom.left + dom.width / 2) - (canvas.width / 2);
   const y = -(dom.top + dom.height / 2) + (canvas.height / 2);
-  // 戻り値がオブジェクト
   return { x, y };
 }
 
@@ -296,7 +233,7 @@ function bindResizeEvents() {
 }
 
 function resizeMesh(mesh_obj, newCanvasRect) {
-  const { $: { el }, mesh, plane_geometry, rect } = mesh_obj;
+  const { $: { el }, mesh, geometry, rect } = mesh_obj;
   const newRect = el.getBoundingClientRect();
   // DOMと同じ座標にする
   const { x, y } = getWorldPosition(newRect, newCanvasRect);
@@ -304,7 +241,7 @@ function resizeMesh(mesh_obj, newCanvasRect) {
   mesh.position.y = y;
 
   // メッシュのサイズも変更する
-  plane_geometry.scale(newRect.width / rect.width, newRect.height / rect.height, 1);
+  geometry.scale(newRect.width / rect.width, newRect.height / rect.height, 1);
   // mesh_obj.rectをリサイズ後の値で更新しておく
   mesh_obj.rect = newRect;
 }
@@ -343,3 +280,18 @@ function _attachStatsJs() {
   });
 }
 // stats.jsのハンドラ関数（ここまで）-----------------------
+
+// // scrollTriggerの初期化処理
+// function initInview(){
+//   gsap.registerPlugin(ScrollTrigger);
+//   const elements = document.querySelectorAll('[data-webgl]'); 
+//   elements.forEach(el => {
+//     gsap.to(el, {
+//       x: 100,
+//       scrollTrigger: {
+//         trigger: el,
+//         start: "center 60%"
+//       }
+//     });
+//   });
+// }
